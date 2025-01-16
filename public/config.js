304facf75c76e9102d6e4d9eeed44f64bb5b08c5
Que6tion <mindaugasv@paynexpay.com>
2025-01-16 14:22:15 +0200

    Update config.js

--- public/config.js
+++ public/config.js
@@ -1,2 +1,2 @@
 // config.js.example
-const API_KEY = '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';
+const API_KEY = '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';
